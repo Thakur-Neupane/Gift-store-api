6b9e3cd79cfbd162816c33e4b668807aeae7a305
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-23 18:42:33 +1000

    fixed to user router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -44,7 +44,7 @@ router.get("/", auth, (req, res, next) => {
       : res.json({
           status: "error",
           message:
-            "Your account has not been activated. Check your email to verify your account",
+            "your account has not been activated. Check your email to verify your account",
         });
   } catch (error) {
     next(error);
@@ -53,13 +53,13 @@ router.get("/", auth, (req, res, next) => {
 
 router.post("/", newUserValidation, async (req, res, next) => {
   try {
-    // Encrypt password
+    // encrypt password
     req.body.password = hashPassword(req.body.password);
 
     const user = await insertUser(req.body);
 
     if (user?._id) {
-      // Create unique URL and add in the database
+      // create unique url and add in the database
       const token = uuidv4();
       const obj = {
         token,
@@ -68,7 +68,8 @@ router.post("/", newUserValidation, async (req, res, next) => {
 
       const result = await insertSession(obj);
       if (result?._id) {
-        // Process for sending email
+        //process for sending email
+
         emailVerificationMail({
           email: user.email,
           fName: user.fName,
@@ -78,12 +79,12 @@ router.post("/", newUserValidation, async (req, res, next) => {
         return res.json({
           status: "success",
           message:
-            "We have sent you an email with instructions to verify your account. Please check your email/junk folder to verify your account",
+            "We have send you an email with insturction to verify your  account. Pelase chekc email/junk to verify your account",
         });
       }
     }
 
-    res.status(500).json({
+    res.json({
       status: "error",
       message: "Error Unable to create an account, Contact administration",
     });
@@ -92,26 +93,27 @@ router.post("/", newUserValidation, async (req, res, next) => {
   }
 });
 
-// User verification
+//user verification
 router.post("/user-verification", async (req, res, next) => {
   try {
     const { c, e } = req.body;
-    // Delete session data
+    //delete session data
+
     const session = await deleteSession({
       token: c,
       associate: e,
     });
     if (session?._id) {
-      // Update user table
+      //update user table
       const result = await updateUser(
         { email: e },
         {
-          status: "active", // Fixed typo here
+          staus: "active",
           isEmailVerified: true,
         }
       );
       if (result?._id) {
-        // Send user an email
+        // send user an email
         return res.json({
           status: "success",
           message: "Your account has been verified. You may sign in now",
@@ -119,7 +121,7 @@ router.post("/user-verification", async (req, res, next) => {
       }
     }
 
-    res.status(400).json({
+    res.json({
       status: "error",
       message: "Invalid link, contact admin",
     });
@@ -129,23 +131,27 @@ router.post("/user-verification", async (req, res, next) => {
 });
 
 // Admin authentication
+
 router.post("/login", async (req, res, next) => {
   try {
     let message = "";
     const { email, password } = req.body;
-    // 1. Check if user exists with email
+    // 1. cheich if user exist with email
     const user = await getAUser({ email });
 
     if (user?._id && user?.status === "active" && user?.isEmailVerified) {
-      // Verify passwords
+      //verify passwords
+
       const confirmPass = comparePassword(password, user.password);
 
       if (confirmPass) {
-        // User is now authenticated
-        // Create JWTs then return
+        //useris now authenticated
+
+        // create jwts then return
+
         return res.json({
           status: "success",
-          message: "Login successful",
+          message: "Login Successfull",
           jwts: await getTokens(email),
         });
       }
@@ -159,7 +165,7 @@ router.post("/login", async (req, res, next) => {
       message = "User not verified, please check your email and verify";
     }
 
-    res.status(401).json({
+    res.json({
       status: "error",
       message: message || "Invalid login details",
     });
@@ -168,22 +174,25 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-// Return new accessJWT
+// return new accessJWT
 router.get("/new-accessjwt", async (req, res, next) => {
   try {
     const { authorization } = req.headers;
 
-    // Verify JWT
+    // verify jwt
+
     const decode = verifyRefreshJWT(authorization);
+    console.log(decode, "--------");
     if (decode?.email) {
-      // Check if exists in the user table
+      // check if exist in the user table,
       const user = await getAUser({
         email: decode.email,
         refreshJWT: authorization,
       });
 
       if (user?._id) {
-        // Create new accessJWT and return
+        // create new accessJWT and return
+
         const accessJWT = await signAccessJWT(decode.email);
 
         if (accessJWT) {
@@ -205,7 +214,7 @@ router.get("/new-accessjwt", async (req, res, next) => {
   }
 });
 
-// Getting all users
+// getting all users
 router.get("/all", auth, async (req, res, next) => {
   try {
     const users = await getAllUsers();
@@ -224,22 +233,22 @@ router.delete("/logout", auth, async (req, res, next) => {
     const { email } = req.userInfo;
 
     await updateUser({ email }, { refreshJWT: "" });
-    // Verify JWT
+    // verify jwt
     await deleteManySession({ associate: email });
 
     res.json({
       status: "success",
-      message: "You are logged out",
+      message: "you are loggedout",
     });
   } catch (error) {
     next(error);
   }
 });
-
-// Request OTP for password reset
+// request OTP for password reset
 router.post("/otp", async (req, res, next) => {
   try {
     const { email } = req.body;
+    console.log(req.body, "lll");
 
     const user = await getAUser({ email });
 
@@ -252,31 +261,56 @@ router.post("/otp", async (req, res, next) => {
         type: "otp",
       });
 
-      // Send the email
+      //send the email
       sendOTPMail({ token, fName: user.fName, email });
-
-      return res.json({
-        status: "success",
-        message:
-          "If your email is found in our system, we have sent you an OTP to your email. Please check your Inbox/Junk folder",
-      });
     }
 
-    res.status(404).json({
-      status: "error",
-      message: "Email not found",
+    res.json({
+      status: "success",
+      message:
+        "If your email is found in our system, We have sent you an OTP in your email, please check your Inbox/Junk folder",
     });
   } catch (error) {
     next(error);
   }
 });
+// request OTP for password reset
+router.post("/otp", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    console.log(req.body, "lll");
 
-// Reset password
+    const user = await getAUser({ email });
+
+    if (user?._id) {
+      const token = otpGenerator();
+
+      await insertSession({
+        token,
+        associate: email,
+        type: "otp",
+      });
+
+      //send the email
+      sendOTPMail({ token, fName: user.fName, email });
+    }
+
+    res.json({
+      status: "success",
+      message:
+        "If your email is found in our system, We have sent you an OTP in your email, please check your Inbox/Junk folder",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+// request OTP for password reset
 router.patch("/password/reset", async (req, res, next) => {
   try {
     const { email, otp, password } = req.body;
-    if (email && otp && password) {
-      // Verify OTP is valid
+    if ((email, otp, password)) {
+      // verify otp is valid
+
       const session = await deleteSession({
         token: otp,
         associate: email,
@@ -284,26 +318,27 @@ router.patch("/password/reset", async (req, res, next) => {
       });
 
       if (session?._id) {
-        // Update user table with new hashed password
+        //update user table with new hashPass
+
         const user = await updateUser(
           { email },
           { password: hashPassword(password) }
         );
 
         if (user?._id) {
-          // Send email notification of account update
+          //send email notification of account update
           accountUpdatedNotification({ email, fName: user.fName });
           return res.json({
             status: "success",
-            message: "Your password has been reset successfully",
+            message: "Your password reseted successfully",
           });
         }
       }
     }
 
-    res.status(400).json({
+    res.json({
       status: "error",
-      message: "Invalid OTP or data request, try again later",
+      message: "Invalid otp or data reqeust, try agian later",
     });
   } catch (error) {
     next(error);
