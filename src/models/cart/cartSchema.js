e881a550793f3d8a71bc1f49e0387b9ab37ce87e
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-09-04 01:22:44 +1000

    scheam for th e cart please

--- src/models/cart/cartSchema.js
+++ src/models/cart/cartSchema.js
@@ -26,7 +26,7 @@ const cartSchema = new mongoose.Schema(
         },
         title: {
           type: String,
-          //   required: true,
+          // required: true,
         },
       },
     ],
@@ -42,6 +42,40 @@ const cartSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+    },
+    address: {
+      unitNumber: {
+        type: String,
+        required: true,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      zipCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+      message: {
+        type: String,
+        default: "",
+      },
     },
   },
   {
