03b1adda3d5e710e10f1bc46e858cec563978d02
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-09-17 22:23:18 +1000

    order schema

---
+++ src/models/orders/orderSchema.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: {
+          type: Number,
+          required: true,
+        },
+        color: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        size: {
+          type: String,
+        },
+        title: {
+          type: String,
+        },
+      },
+    ],
+
+    paymentIntent: {},
+
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+        "Completed",
+      ],
+    },
+
+    orderedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Order", orderSchema);
