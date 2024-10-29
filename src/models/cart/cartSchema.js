6a6cde6f8b2f5cbb93c3e9475b2990d9d77c3aaa
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-10-29 12:02:48 +1100

    done

--- src/models/cart/cartSchema.js
+++ src/models/cart/cartSchema.js
@@ -7,34 +7,42 @@ const cartSchema = new mongoose.Schema(
         product: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "Product",
+          required: true,
         },
         count: {
           type: Number,
           required: true,
+          min: 1,
         },
         color: {
           type: String,
           required: true,
+          trim: true,
         },
         price: {
           type: Number,
           required: true,
+          min: 0,
         },
         size: {
           type: String,
+          trim: true,
         },
         title: {
           type: String,
+          trim: true,
         },
       },
     ],
     cartTotal: {
       type: Number,
       required: true,
+      min: 0,
     },
     totalAfterDiscount: {
       type: Number,
       default: 0,
+      min: 0,
     },
     orderedBy: {
       type: mongoose.Schema.Types.ObjectId,
@@ -42,19 +50,40 @@ const cartSchema = new mongoose.Schema(
       required: true,
     },
     address: {
-      unitNumber: { type: String },
-      street: { type: String },
-      city: { type: String },
-      state: { type: String },
-      zipCode: { type: String },
-      country: { type: String },
-      phoneNumber: { type: String },
-      message: { type: String },
+      unitNumber: { type: String, trim: true },
+      street: { type: String, trim: true },
+      city: { type: String, trim: true },
+      state: { type: String, trim: true },
+      zipCode: { type: String, trim: true },
+      country: { type: String, trim: true },
+      phoneNumber: {
+        type: String,
+        trim: true,
+        validate: {
+          validator: function (v) {
+            return /\+?[0-9]{7,15}/.test(v);
+          },
+          message: (props) => `${props.value} is not a valid phone number!`,
+        },
+      },
+      message: { type: String, trim: true },
     },
   },
   {
     timestamps: true,
   }
 );
+
+// Pre-save hook to ensure cart total remains accurate
+cartSchema.pre("save", function (next) {
+  this.cartTotal = this.products.reduce(
+    (total, item) => total + item.count * item.price,
+    0
+  );
+  if (this.totalAfterDiscount > this.cartTotal) {
+    this.totalAfterDiscount = this.cartTotal;
+  }
+  next();
+});
 
 export default mongoose.model("Cart", cartSchema);
